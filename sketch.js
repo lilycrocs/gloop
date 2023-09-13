71cd7f76e8a77611d2cf72929cecc819edb43aa9
LILY CROCAMO <LC22BG@FSU.EDU>
2023-09-13 11:32:33 -0400

    ping pong

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
+let x = 200;
+let speed = 5;
+let diameter = 100;
+
 function setup() {
   createCanvas(400, 400);
+  noStroke()
 }
 
 function draw() {
-  background(125);
+  background(125, 200, 0);
+  ellipse(x, height/2, diameter);
+  if (x >= width - diameter/2 || x <= 0 - diameter/2 + 100) {
+    speed = speed * -1;
+  }
+  x += speed;
 }
