e30cd3d153d666a13a6026e4abf02821767b1f75
LILY CROCAMO <LC22BG@FSU.EDU>
2023-09-20 00:42:33 -0400

    things to try

--- sketch.js
+++ sketch.js
@@ -1,17 +1,49 @@
-let x = 200;
-let speed = 5;
+let ballx = 200;
+let rectx = 200
+let ballspeed = 5;
 let diameter = 100;
+let move = true;
+let gray = 125; 
 
 function setup() {
   createCanvas(400, 400);
   noStroke()
+  rectMode(CENTER)
 }
 
 function draw() {
-  background(125, 200, 0);
-  ellipse(x, height/2, diameter);
-  if (x >= width - diameter/2 || x <= 0 - diameter/2 + 100) {
-    speed = speed * -1;
+  if (millis() > 10000) {
+    background("orange");
+  } else {
+    background(125, 200, 0);
   }
-  x += speed;
+  if (ballx >= width - diameter/2 || ballx <= 0 - diameter/2 + 100) {
+    ballspeed = ballspeed * -1;
+  }
+  if (move) {
+    ballx=ballx+ballspeed
+  }
+  fill(gray)
+  ellipse(ballx, height/2, diameter);
+  fill(90, 0, 125, frameCount)
+  rect(rectx, height/5, 90)
+}
+
+function mouseClicked() {
+  if (move) {
+    move = false;
+  } else {
+    move = true;
+    gray = random(255);
+  }
+}
+
+function keyPressed() {
+  if (key == 'j' || key == 'J') {
+    if (ballspeed < 0) {
+      console.log("left");
+    } else {
+      console.log("right");
+    }    
+  }
 }
